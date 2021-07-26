0b1d66aef897e55b01069625a9406fa933a811d4
jungwoon <jungwoon018@gmail.com>
2021-07-26 12:06:10 +0900

    프로젝트 생성

---
+++ src/main/java/com/jungwoon/tistory_clone_springboot/TistoryCloneSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jungwoon.tistory_clone_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TistoryCloneSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TistoryCloneSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jungwoon/tistory_clone_springboot/TistoryCloneSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungwoon.tistory_clone_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TistoryCloneSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
