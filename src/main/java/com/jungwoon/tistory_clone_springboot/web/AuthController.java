9a05bd377093cbaf2a15238c7e68ea407bb738dc
jungwoon <jungwoon018@gmail.com>
2021-07-26 20:34:15 +0900

    index 템플릿 레이아웃 구성 완성

--- src/main/java/com/jungwoon/tistory_clone_springboot/web/AuthController.java
+++ src/main/java/com/jungwoon/tistory_clone_springboot/web/AuthController.java
@@ -1,9 +1,13 @@
 package com.jungwoon.tistory_clone_springboot.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
 
-
+    @GetMapping("/auth/login")
+    public String login() {
+        return "login";
+    }
 }
