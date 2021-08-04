b3724f096725aed24197172633446a272b6e6fe2
jungwoon <jungwoon018@gmail.com>
2021-08-04 22:14:59 +0900

    카테고리 추가 템플릿 완성

--- src/main/java/com/jungwoon/tistory_clone_springboot/web/BlogController.java
+++ src/main/java/com/jungwoon/tistory_clone_springboot/web/BlogController.java
@@ -1,18 +1,12 @@
 package com.jungwoon.tistory_clone_springboot.web;
 
-import com.jungwoon.tistory_clone_springboot.config.oauth.dto.PrincipalDetails;
 import com.jungwoon.tistory_clone_springboot.service.BlogService;
-import com.jungwoon.tistory_clone_springboot.web.dto.blog.BlogCreateRequestDto;
 import com.jungwoon.tistory_clone_springboot.web.dto.blog.BlogManageRespDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -25,7 +19,6 @@ public class BlogController {
     public String blogManage(@PathVariable String url, Model model) {
 
         BlogManageRespDto blogManageRespDto = blogService.blogManage(url);
-
         model.addAttribute("blog", blogManageRespDto);
 
         return "blog/manage";
@@ -37,8 +30,13 @@ public class BlogController {
         return "blog/manage-newpost";
     }
 
+    // 카테고리 관리 페이지
     @GetMapping("/{url}/manage/category")
-    public String manageCategory(@PathVariable String url) {
-        return null;
+    public String manageCategory(@PathVariable String url, Model model) {
+
+        BlogManageRespDto blogManageRespDto = blogService.blogManage(url);
+        model.addAttribute("blog", blogManageRespDto);
+
+        return "blog/manage-category";
     }
 }
